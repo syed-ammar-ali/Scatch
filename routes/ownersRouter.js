1076c13c1ec72fd1d78c2b26426d708ca8d69923
Ammar <syedammarali12a@gmail.com>
2024-09-29 11:48:22 +0530

    added development json. removed isAdmin field from user-model. created index route. modified ownerRouter, created some check flags. modified userRouter, implimented try catch.

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res
+        .status(504)
+        .send("you dont have permission to create a new owner");
+    }
+
+    let { fullname, email, password } = req.body;
+
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+
+    res.status(201).send(createdOwner);
+  });
+}
 
 router.get("/", function (req, res) {
   res.send("heys");
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require("../models/user-model");
 
 router.get("/", function (req, res) {
   res.send("heys");
+});
+
+router.post("/register", async function (req, res) {
+  try {
+    let { fullname, email, password } = req.body;
+    let user = await userModel.create({
+      fullname,
+      email,
+      password,
+    });
+    res.send(user);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 module.exports = router;
